fc8dbc6c46fb4da7b9e5b0af3b509f124bf44075
Drod86 <daniel.r.rodriguez86@gmail.com>
2023-05-24 20:28:50 -0500

    add painting in rainbow and shades of gray and buttons to choose the paint color and clear the sketch

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const BODY = document.querySelector("body");
-BODY.innerHTML += `<button class="gridSize">Change the grid size here.</button><button class="clear">Clear</button>`;
+BODY.innerHTML += `<button class="gridSize">Change the grid size here.</button><button class="clear">Clear</button><div class='chooseColor'><h3>Choose paint color:</h3><button class="chooseBlack">Black</button><button class="rainbow">Rainbow</button><button class="shadesOfGray">Shades of Gray</button></div>`;
 
-let gridSize = 100;
-const gridContainer = document.querySelector(".container");
-
+// Create the Grid
 const addGridItems = (num) => {
   let numItems = num * num;
   let items = "";
@@ -12,17 +10,61 @@ const addGridItems = (num) => {
   }
   return items;
 };
-
+let gridSize = 100;
+const gridContainer = document.querySelector(".container");
 gridContainer.innerHTML = addGridItems(gridSize);
 
+// Sketch functionality -- manipulate background color
+// functions to manipulate the background
+const toBlack = (div) => div.classList.add("black");
+const toRandomColor = (div) => {
+  const random =
+    Math.floor(Math.random() * 180) + Math.floor(Math.random() * 180);
+  div.setAttribute("style", `background-color: hsla(${random}, 100%, 50%, 1);`);
+};
+const toShadesOfGray = (div) => {
+  const isShaded = div.attributes.length;
+  if (isShaded === 1) {
+    div.setAttribute("style", `background-color: hsla(0, 0%, ${90}%, 1);`);
+  } else {
+    for (const attr of div.attributes) {
+      if (attr.name === "style") {
+        const light = Number(attr.value.substr(30, 2));
+        if (!isNaN(light))
+          div.setAttribute(
+            "style",
+            `background-color: hsla(0, 0%, ${light - 10}%, 1);`
+          );
+      }
+    }
+  }
+};
+
+// choose the color to sketch with
+const blackBtn = document.querySelector(".chooseBlack");
+const rainbowBtn = document.querySelector(".rainbow");
+const shadesOfGrayBtn = document.querySelector(".shadesOfGray");
+let paintColor = 0;
+const colorChoices = [blackBtn, rainbowBtn, shadesOfGrayBtn];
+colorChoices.forEach((btn, i) =>
+  btn.addEventListener("click", () => (paintColor = i))
+);
+
+//grab the grid cells
 const divs = document.querySelectorAll(".item");
 
+// Sketch functionality
 divs.forEach((div) =>
-  div.addEventListener("mouseover", () => {
-    div.classList.add("black");
+  div.addEventListener("mouseover", (e) => {
+    paintColor === 0
+      ? toBlack(div)
+      : paintColor === 1
+      ? toRandomColor(div)
+      : toShadesOfGray(div);
   })
 );
 
+// button to change the grid size or pixel scale
 const changeGridSize = () => {
   divs.forEach((div) => div.classList.remove("black"));
   gridSize = parseInt(
@@ -52,8 +94,12 @@ const changeGridSizeBtn = document.querySelector(".gridSize");
 
 changeGridSizeBtn.addEventListener("click", changeGridSize);
 
+/// clear
 const clearBtn = document.querySelector(".clear");
 
 clearBtn.addEventListener("click", () => {
-  divs.forEach((div) => div.classList.remove("black"));
+  divs.forEach((div) => {
+    div.classList.remove("black");
+    div.attributes.length > 1 && div.attributes.removeNamedItem("style");
+  });
 });
